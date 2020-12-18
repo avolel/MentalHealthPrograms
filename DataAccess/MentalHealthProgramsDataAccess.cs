8bd8a1f67997808002aae2f76f4b48aa70d4eb54
AVolel <avolel@gmail.com>
2020-12-18 10:14:15 -0500

    Model added to solution

--- DataAccess/MentalHealthProgramsDataAccess.cs
+++ DataAccess/MentalHealthProgramsDataAccess.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Configuration;
-using Models;
+using Model;
 using System.Data;
 using System.Data.SqlClient;
 using Logger;
---
+++ Model/MentalHealthProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class MentalHealthProgram
+    {
+        public string Name_1 { get; set; }
+        public string Name_2 { get; set; }
+        public string Street_1 { get; set; }
+        public string Street_2 { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+    }
+}
